23f57cf72cba27b739da73fe0826295cd97f24c8
RcodIOS <m.ismayilov559@gmail.com>
2023-01-11 21:05:45 +0400

    First Commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,6 +14,60 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnAC(_ sender: Any) {
+    }
+    
+   
+    @IBAction func btn7(_ sender: Any) {
+    }
+    @IBAction func btn4(_ sender: Any) {
+    }
+    
+    @IBAction func btn1(_ sender: Any) {
+    }
+    
+    
+    @IBAction func btn2(_ sender: Any) {
+    }
+    
+    
+    @IBAction func btn5(_ sender: Any) {
+    }
+    
+    
+    @IBAction func btn8(_ sender: Any) {
+    }
+    
+    
+    @IBAction func btn3(_ sender: Any) {
+    }
+    
+    
+    @IBAction func btn6(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func btn9(_ sender: Any) {
+    }
+    
+    
+    @IBAction func btnEquals(_ sender: Any) {
+    }
+    
+    
+    @IBAction func btnTopla(_ sender: Any) {
+    }
+    
+    
+    @IBAction func btnCix(_ sender: Any) {
+    }
+    
+    
+    @IBAction func btnVur(_ sender: Any) {
+    }
+    
+    
+    @IBOutlet weak var btnBol: UILabel!
 }
 
